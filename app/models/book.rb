c112dc437d5813720dd7bf4ed39b5776e5842e53
Gabriel Takashi Katakura <gabriel.takashi@bysoft.com.br>
2015-11-11 14:17:55 -0200

    Exclusão de Livro
    Analisando como deve ser feito a exclusão de um Livro.
    Sendo feito commit previamente para verificar se o .gitignore será
    validado.

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,10 +9,27 @@ class Book < ActiveRecord::Base
 	validates :pages, numericality: { only_integer: true, greater_than: 0 }
 	validates :copies, numericality: { only_integer: true, greater_than: 0 }
 	validates :book_binding, inclusion: { in: [0, 1] }
+	validate :copies_is_less_than_loans_opened
+
+	def before_destroy
+		if self.loans.empty?
+			true
+		else
+			errors.add(:base, "Cannot delete booking with loans")
+			false
+		end
+	end
 
 	def has_in_inventory?
 		self.copies > self.loans.opened.count
 	end
 
 	paginates_per 5
+	
+protected
+	def copies_is_less_than_loans_opened
+		if self.copies < self.loans.opened.count
+			errors.add(:copies, "is smaller than the amount of open loans")
+		end
+	end
 end
